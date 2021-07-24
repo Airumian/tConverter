2fa71deac6f4c956f9d8295d6fed590249bf2651
Alexander <Airumian>
2021-07-24 17:47:48 +0300

    First

--- tConverter/ViewController.swift
+++ tConverter/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var cellsium: UILabel!
+    @IBOutlet weak var farengeit: UILabel!
+    @IBOutlet weak var slider: UISlider!{
+        didSet {
+            slider.minimumValue = 0
+            slider.maximumValue = 100
+            slider.value = 0
+        }
     }
-
-
+    
+    @IBAction func sladerChange(_ sender: UISlider) {
+        let tempCellsium = Int(round(slider.value))
+        cellsium.text = "\(tempCellsium) Cº"
+        let tempFarengeit = Int((round(slider.value * 9 / 5) + 32))
+        farengeit.text = "\(tempFarengeit) Fº"
+    }
 }
-
